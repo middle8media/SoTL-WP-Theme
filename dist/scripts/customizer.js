67dd0f92f1f3071e3c2d084ca27868151162dfc5
Seth Hall <seth@ushahidi.com>
2016-06-08 11:55:44 -0400

    adds .gitignore file/rebuilds

---
+++ dist/scripts/customizer.js
@@ -0,0 +1,2 @@
+!function(n){wp.customize("blogname",function(e){e.bind(function(e){n(".brand").text(e)})})}(jQuery),function(n){console.log(n),console.log("wrap test"),n(".single-news .entry-content iframe").hide()}(jQuery);
+//# sourceMappingURL=customizer.js.map
